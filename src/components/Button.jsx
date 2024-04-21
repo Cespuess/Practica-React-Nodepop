7ff6b0b5bff5b918e37f4e25d333217b2fef8091
Cespuess <cespuess@gmail.com>
2024-04-21 14:26:21 +0200

    habilitación y deshabilitación del botón del form.

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,9 +1,19 @@
 import PropTypes from 'prop-types';
 import styles from './components-styles/Button.module.scss';
 
-export default function Button({ buttonType, eventFunction, children }) {
+export default function Button({
+  buttonType,
+  eventFunction,
+  children,
+  disabledButton = false
+}) {
   return (
-    <button type={buttonType} className={styles.button} onClick={eventFunction}>
+    <button
+      type={buttonType}
+      className={`${styles.button} ${disabledButton ? styles.disabled : ''}`}
+      onClick={eventFunction}
+      disabled={disabledButton}
+    >
       {children}
     </button>
   );
@@ -12,5 +22,6 @@ export default function Button({ buttonType, eventFunction, children }) {
 Button.propTypes = {
   buttonType: PropTypes.string,
   children: PropTypes.string.isRequired,
-  eventFunction: PropTypes.func
+  eventFunction: PropTypes.func,
+  disabledButton: PropTypes.bool
 };
--- src/pages/auth/LoginPage.jsx
+++ src/pages/auth/LoginPage.jsx
@@ -1,8 +1,24 @@
+import { useState } from 'react';
 import Button from '../../components/Button';
 import Input from '../../components/Input';
 import styles from './LoginPage.module.scss';
 
 export default function LoginPage() {
+  const [formValues, setFormValues] = useState({
+    email: '',
+    password: ''
+  });
+
+  const { email, password } = formValues;
+  const disButton = !email || !password;
+
+  const handleChange = (event) => {
+    setFormValues((currentFormValues) => ({
+      ...currentFormValues,
+      [event.target.name]: event.target.value
+    }));
+  };
+
   return (
     <div className={styles.loginPage}>
       <h2>
@@ -12,12 +28,16 @@ export default function LoginPage() {
         <Input
           inputType="email"
           inputName="email"
+          inputValue={email}
           placeholderText="Introduce tu e-mail"
+          onChangeFunction={handleChange}
         />
         <Input
           inputType="password"
           inputName="password"
+          inputValue={password}
           placeholderText="Contraseña"
+          onChangeFunction={handleChange}
         />
         <div className={styles.checkbox_container}>
           <input
@@ -27,7 +47,9 @@ export default function LoginPage() {
           ></input>
           <label htmlFor="rememberPassword">Recordar contraseña</label>
         </div>
-        <Button buttonType="submit">Acceder a Nodepop</Button>
+        <Button buttonType="submit" disabledButton={disButton}>
+          Acceder a Nodepop
+        </Button>
       </form>
     </div>
   );
