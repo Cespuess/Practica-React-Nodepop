be34b0331c5b1771a190bacb26e89c9e536310cd
Cespuess <cespuess@gmail.com>
2024-04-21 18:50:49 +0200

    url de la api en el .env, contexto para isLogged creado, y cambios en el Header según el estado logeado o no.

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,13 +1,23 @@
+import { useAuth } from '../../pages/auth/authContext';
 import Button from '../Button';
 import styles from './layout-styles/Header.module.scss';
 
 export default function Header() {
+  const { isLogged, onLogout } = useAuth();
+
   return (
     <header className={styles.header}>
       <h1>NODEPOP</h1>
       <nav>
-        <Button>Anuncios</Button>
-        <Button>Crear Anuncio</Button>
+        {isLogged ? (
+          <>
+            <Button>Anuncios</Button>
+            <Button>Crear Anuncio</Button>
+            <Button eventFunction={onLogout}>Logout</Button>
+          </>
+        ) : (
+          <p>No hay ningún usuario logeado</p>
+        )}
       </nav>
     </header>
   );
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './main.module.scss';
+import { setAuthorizationHeader } from './api/client.js';
+import storage from './utils/storage.js';
+import { AuthContextProvider } from './pages/auth/authContext.jsx';
+
+const accessToken = storage.get('auth');
+if (accessToken) setAuthorizationHeader(accessToken);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <AuthContextProvider isDefaultLogged={!!accessToken}>
+      <App />
+    </AuthContextProvider>
   </React.StrictMode>
 );
